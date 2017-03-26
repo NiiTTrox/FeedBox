08802af694c9802a2fdef31cd2528015811842f3
Felix Maaß <felix@maass24.de>
2017-03-26 20:11:55 +0200

    php implementiert

--- test.php
+++ test.php
@@ -1,28 +1,49 @@
 <?php
 
+$servername = "localhost";
+$username = "testy";
+$password = "1234";
+$dbname = "feedbox";
 
-$con=mysqli_connect("localhost","testy","1234","feedbox");
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, 3306);
 // Check connection
-if (mysqli_connect_errno())
-{
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
-$sql="
-  SELECT users.usern, feeds.content, feeds.date
-  FROM feeds 
-  JOIN users ON feeds.userid == users.id
-  ORDER BY feeds.date
-  ";
-$result=mysqli_query($con,$sql);
+$sql = "
+SELECT users.username, feeds.content, feeds.date
+FROM feeds
+JOIN users ON feeds.userid = users.id
+ORDER BY feeds.date DESC
+";
 
-// Fetch all
-mysqli_fetch_all($result,MYSQLI_ASSOC);
+$result = $conn->query($sql);
 
-// Free result set
-mysqli_free_result($result);
 
-mysqli_close($con);
-?>
+
+
+if ($result->num_rows > 0) {
+    // output data of each row
+
+
+    echo "<style>tr:nth-child(odd){ background: cornflowerblue; } tr:nth-child(even) {background: darkseagreen}</style>";
+
+    echo "<table style='border: 1px solid red;'>";
+
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["username"]. "</td><td>" . $row["content"]. "</td><td>" . $row["date"]. "</td></tr>";
+    }
+
+    echo "</table>";
+
+} else {
+    echo "0 results";
+}
+
+
+$conn->close();
+
 
 
